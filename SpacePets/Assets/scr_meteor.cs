3c05ac072412616f451768d3be70121a95bfe09f
David Pierce <davidprc3@gmail.com>
2021-07-09 23:49:44 -0400

    File setup, first pass on texture and object rotation randomization
    Added texture and bumpmap for meteor material
    added basic file structure
    added random rotation script
    left a few test objects rotation around with new material

---
+++ SpacePets/Assets/scr_meteor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_meteor : MonoBehaviour
+{
+
+    float xRotSpeed = 0f;
+    float yRotSpeed = 0f;
+    float zRotSpeed = 0f;
+    public float rotationSpeedRange = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRotSpeed = Random.Range(-1f * rotationSpeedRange, rotationSpeedRange);
+        yRotSpeed = Random.Range(-1f * rotationSpeedRange, rotationSpeedRange);
+        zRotSpeed = Random.Range(-1f * rotationSpeedRange, rotationSpeedRange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xRotSpeed * Time.deltaTime, yRotSpeed * Time.deltaTime, zRotSpeed * Time.deltaTime);
+    }
+}
